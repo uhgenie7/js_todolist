d56d0db2f298c4acb6aea7b63da1917eb979ebea
uhj1993 <dzzd777@naver.com>
2020-11-10 10:46:53 +0900

    Making a JS Clock part

---
+++ practise.js
@@ -0,0 +1,15 @@
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleclick() {
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() {
+    title.addEventListener("click", handleclick);
+    
+}
+
+init();
